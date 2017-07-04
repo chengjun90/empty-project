5e2aa7a9e01ea820b6885031f3d81549a4c7bfa2
Cheng Jun <cheng081@qq.com>
2017-07-04 20:06:16 +0800

    add some packages and list.files
    add some packages and list.files

--- R/1_setup.R
+++ R/1_setup.R
@@ -6,6 +6,13 @@
 # source()和library()语句
 source('')
 library(tidyverse)
+library(data.table)
+library(haven)
+library(readxl)
+library(stringr)
+library(janitor)
+library(skimr)
+library(lubridate)
 
 # 函数定义----
 clean <- function(x, y) {
@@ -19,6 +26,11 @@ clean <- function(x, y) {
 dta <- readxl::read_excel("raw_data/公司信息/info.xlsx")  # 采用相对路径进行
 
 print("ok")
+
+# 列示数据集
+list.files(path = "raw_data/财务指标",
+           pattern = ".xlsx",
+           full.names = TRUE) %>% as_tibble()
 
 # 合并数据----
 print("ok")
